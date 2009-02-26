acf6fba490a2cd680e3efbfdb10e54a5b71e1856
Alexey Noskov <alexey.noskov@gmail.com>
2009-02-26 11:37:55 +0300

    Tag desc

--- lib/code_tags.rb
+++ lib/code_tags.rb
@@ -3,8 +3,14 @@ require 'coderay'
 module CodeTags
   include Radiant::Taggable
 
+  desc %{
+    Highlights an code.
+
+    *Usage*:
+    <pre><code><r:code lang="ruby">...</r:code></pre></code>
+  }
   tag 'code' do |tag|
-    CodeRay.scan( tag.expand, "ruby" ).div( :line_numbers => :table, :css => :style, :style => :cycnus )
+    CodeRay.scan( tag.expand, tag.attr['lang'] || 'ruby' ).div( :line_numbers => :table, :css => :style, :style => :cycnus )
   end
 
 end
