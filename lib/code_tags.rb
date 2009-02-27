fa6d72fcfc9519a49d3798ac6eec672b56d7635c
Alexey Noskov <alexey.noskov@gmail.com>
2009-02-27 11:04:03 +0300

    Lines attr

--- lib/code_tags.rb
+++ lib/code_tags.rb
@@ -7,10 +7,10 @@ module CodeTags
     Highlights an code.
 
     *Usage*:
-    <pre><code><r:code lang="ruby">...</r:code></pre></code>
+    <pre><code><r:code lang="ruby" lines="table">...</r:code></pre></code>
   }
   tag 'code' do |tag|
-    CodeRay.scan( tag.expand, tag.attr['lang'] || 'ruby' ).div( :line_numbers => :table, :css => :style, :style => :cycnus )
+    CodeRay.scan( tag.expand, tag.attr['lang'] || 'ruby' ).div( :line_numbers => tag.attr['lines'], :css => :style, :style => :cycnus )
   end
 
 end
