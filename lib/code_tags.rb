8b3b2aa7b55a4745754c7f5ccc70e7e6046c5e2a
Alexey Noskov <alexey.noskov@gmail.com>
2009-04-03 13:12:24 +0400

    Remove first newline in code

--- lib/code_tags.rb
+++ lib/code_tags.rb
@@ -8,7 +8,7 @@ module CodeTags
     <pre><code><r:code lang="ruby" lines="table">...</r:code></pre></code>
   }
   tag 'code' do |tag|
-    code_processor.highlight( tag.expand, :lang => tag.attr['lang'], :lines => tag.attr['lines'] )
+    code_processor.highlight( tag.expand.sub(/^\n/,''), :lang => tag.attr['lang'], :lines => tag.attr['lines'] )
   end
 
   desc %{
